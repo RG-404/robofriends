3dc951d6839bdca8186cfe9937dd8399ce9c6b3e
RG-404 <55778353+RG-404@users.noreply.github.com>
2020-04-07 16:07:59 +0530

    Done

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
     return ( 
         <div className='pa2'>
-            <input 
+            <input
             className='pa3 ba b--green bg-lightest-blue'
             type='search' 
             placeholder='searh robot'
